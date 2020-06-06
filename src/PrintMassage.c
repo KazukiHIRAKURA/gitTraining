7000e8c57bee2541b8df7a2c9ac2f4340071c13e
Kazuki HIRAKURA <kazuki.hirakura@gmail.com>
2020-06-06 11:21:06 +0900

    ファイルから文字列を取得・表示する

--- src/PrintMassage.c
+++ src/PrintMassage.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#define MAX_LINE 1024
+#define MAX_COLS 80
 
 int main(void){
+	char str[MAX_COLS];
+	FILE *fp;
+	fp = fopen("prop/prop1.dat","r");
+	if(fp == NULL){
+		printf("Can not open the file.\n");
+		return 1;
+	}
+	while(fgets(str,MAX_LINE,fp)!=NULL){
+		printf("%s", str);
+	}
+	fclose(fp);
 	printf("This massage is written on the source code.\n");
 	return 0;
 }
